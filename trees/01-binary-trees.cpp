67df3746c7a89825b21698936cbf088e36dec3d3
kartikey <kartikvaghasiya4477@gmail.com>
2024-01-14 10:33:47 +0530

    added method to construct binary trees by user input

--- trees/01-binary-trees.cpp
+++ trees/01-binary-trees.cpp
@@ -22,29 +22,57 @@ class Node {
     }
 };
 
-void constructBinaryTree() {
-    // creating root node & initializing it
-    // data = -1 means no node connected to the either parent's right or left side  
-    // then addind nodes to the right of that node 
+Node* constructBinaryTree(Node* node) {
+
+    int leftData, rightData;
+
+    cout << "data of left node of " << node -> data << ":  ";
+    cin >> leftData;
+    cout << endl;
+
+    // creating left node and linking with parent node
+    Node* newLeftNode = new Node(leftData);
+    node -> left = newLeftNode;
+
+    // if it's not leaf node then... 
+    // call "constructBinaryTree" for "newLeftNode"
+    if(leftData != -1 ) {constructBinaryTree(newLeftNode);}
+
+    cout << "data of right node of " << node -> data << ":  ";
+    cin >> rightData;
+    cout << endl;
+
+    // creating right node and linking with parent node
+    Node* newRightNode = new Node(rightData);
+    node -> right = newRightNode;
+
+    // if it's not leaf node then... 
+    // call "constructBinaryTree" for "newLeftNode"
+    if(rightData != -1 ) {constructBinaryTree(newRightNode);}
+   
+    return node;
 }
 
 int main() {
 
-    Node* root = new Node();
-    root -> left  = new Node(1);
-    root -> right = new Node(2);
+    // Method - 1
+    // Node* root = new Node();
+    // root -> left  = new Node(1);
+    // root -> right = new Node(2);
 
-    root -> left -> left = new Node(11);
-    root -> left -> right = new Node(22);
+    // root -> left -> left = new Node(11);
+    // root -> left -> right = new Node(22);
 
-    cout << "root: " << root -> data << endl;
-    cout << "left of root: " << root -> left -> data << endl;
-    cout << "right of root: " << root -> right -> data << endl;
+    // cout << "root: " << root -> data << endl;
+    // cout << "left of root: " << root -> left -> data << endl;
+    // cout << "right of root: " << root -> right -> data << endl;
 
-    cout << "left of left of root: " << root -> left -> left -> data << endl;
-    cout << "right of left of root: " << root -> left -> right -> data << endl;
+    // cout << "left of left of root: " << root -> left -> left -> data << endl;
+    // cout << "right of left of root: " << root -> left -> right -> data << endl;
 
-
+    // Method - 2
+    Node* root = new Node(0);
+    constructBinaryTree(root);
 
     return 0;
 }
